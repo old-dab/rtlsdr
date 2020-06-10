2dbf2d1712a8ef10122c51deaf841504f99f538d
old-dab <60466067+old-dab@users.noreply.github.com>
2020-06-10 13:32:55 +0200

    Update wavewrite.h

--- src/convenience/wavewrite.h
+++ src/convenience/wavewrite.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 by Kyle Keen <keenerd@gmail.com>
+ * Copyright (C) 2019 by Hayati Ayguen <h_ayguen@web.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
